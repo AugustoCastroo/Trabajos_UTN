a7ff6e1b7a2a4844cc5e6dc518bf823c268b982a
Augusto <au.castro@alumno.um.edu.ar>
2023-11-09 16:26:50 -0300

    comenzamos con JS

--- jsintro/JS/index.js
+++ jsintro/JS/index.js
@@ -9,10 +9,7 @@ const DNI = 12345678; //number
 //alert ("Hola Mundo");
 
 
-
-
-
-console.log('Hola mi nombre es ${nombre} y tengo ${edad} años');
+console.log(`Hola mi nombre es ${nombre} y tengo ${edad} años`);
 console.log('==========================================================')
 
 // Operadores Matemáticos
@@ -44,3 +41,25 @@ console.log(resultado);
 
 resultado = 5 ** 3
 console.log(resultado);
+
+// Contador
+uno++;
+console.log(uno);
+
+uno--;
+console.log(uno);
+
+// Acumulador
+let acumulador = 0;
+console.log(acumulador);
+
+uno += 5;
+console.log(uno);
+
+console.log('==========================================================')
+
+nombre = prompt("Ingrese su nombre");
+
+console.log(`Hola ${nombre} bienvenido a JS`);
+
+alert(`Hola ${nombre} bienvenido a JS`);
