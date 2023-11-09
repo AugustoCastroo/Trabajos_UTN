2ff274205bbf6a79e74f36da1d4e294e303727b8
Augusto <au.castro@alumno.um.edu.ar>
2023-11-09 15:29:19 -0300

    .

--- gitignore/JS/index.js
+++ gitignore/JS/index.js
@@ -5,3 +5,42 @@ var nombre = "Augusto";
 let edad = 21;  //number
 let apellido = "Castro";
 const DNI = 12345678; //number
+
+//alert ("Hola Mundo");
+
+
+
+
+
+console.log('Hola mi nombre es ${nombre} y tengo ${edad} años');
+console.log('==========================================================')
+
+// Operadores Matemáticos
+// + - * / % **
+
+// Operadores Relacionales
+// > < >= <= == === != !==
+
+// Operadores Lógicos
+// && || !
+
+let uno = 500
+let dos = 800
+
+let resultado = uno + dos
+console.log(resultado);
+
+resultado = uno - dos
+console.log(resultado);
+
+resultado = uno * dos
+console.log(resultado);
+
+resultado = uno / dos
+console.log(resultado);
+
+resultado = uno % dos
+console.log(resultado);
+
+resultado = 5 ** 3
+console.log(resultado);
